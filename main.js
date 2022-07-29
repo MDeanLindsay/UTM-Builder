541b3054394734d9604f6ba7c1ee265ef0a7c59e
Michael <michaeldeanlindsay@gmail.com>
2022-07-28 22:02:50 -0700

    hotfixes

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
 function addUrlDecorator() {
-    var connector,
-        url = document.getElementById('url-input').value;
+    var url = document.getElementById('url-input').value;
     if ( url.includes('?') ) {
-      connector = '&'
-    } else {
-      connector = '?'
-    };
-    return url + connector
+      connector = '&';
+    } 
+    else {
+      connector = '?';
+    }
+      return url + connector;
   }
   
 function addUtmSource() {
     var utmSource = document.getElementById('source-input').value;
     if (utmSource != '') {
-      return ('&') + 'utm_source=' + utmSource.split(' ').join('%20')
+      return ('&') + 'utm_source=' + utmSource.split(' ').join('%20');
     }
     else {
-      return ('')
+      return ('');
     }
   }
   
 function addUtmMedium() {
     var utmMedium = document.getElementById('medium-input').value;
     if (utmMedium != '') {
-      return '&utm_medium=' + utmMedium.split(' ').join('%20')
+      return '&utm_medium=' + utmMedium.split(' ').join('%20');
     } 
     else {
-      return ('')
+      return ('');
     }
   }
   
 function addUtmCampaign() {
     var utmCampaign = document.getElementById('campaign-input').value;
     if (utmCampaign != '') {
-      return '&utm_campaign=' + utmCampaign.split(' ').join('%20')
+      return '&utm_campaign=' + utmCampaign.split(' ').join('%20');
     } else {
-      return ''
+      return ('');
     }
   }
   
@@ -43,32 +43,24 @@ function addUtmContent() {
     if (utmContent != '') {
       return '&utm_content=' + utmContent.split(' ').join('%20')
     } else {
-      return ''
+      return ('');
     }
   }
 
-function createUtm() {
-    let utm = addUrlDecorator() + addUtmSource() + addUtmMedium() + addUtmCampaign() + addUtmContent();
-    document.getElementById("utm").value;
-  }
-  
-let data = addUrlDecorator() + addUtmSource() + addUtmMedium() + addUtmCampaign() + addUtmContent();
-
 function myFunction() {
-    
-      if(document.getElementById("url-input").value == '?')
-      {
-        document.getElementById("yourUTM").innerHTML = "You forgot the link."
-      }
-      if(document.getElementById("campaign-input").value.length == 0)
-      {
-        document.getElementById("yourUTM").value = "You forgot the campaign."
-      }
-      else
-        document.getElementById("yourUTM").value = addUrlDecorator() + addUtmSource() + addUtmMedium() + addUtmCampaign() + addUtmContent();
+  if(document.getElementById("url-input").value.length == 0) {
+    document.getElementById("yourUTM").value = "You forgot the link.";
+    }
+  else if(document.getElementById("campaign-input").value.length == 0) {
+    document.getElementById("yourUTM").value = "You forgot the campaign.";
+    }
+  else {
+    document.getElementById("yourUTM").value = addUrlDecorator() + addUtmSource() + addUtmMedium() + addUtmCampaign() + addUtmContent();
+    }
   }
 
 function copyText() {
-  copyText =  document.getElementById("yourUTM").value;
-  navigator.clipboard.writeText(copyText.value);
+  var Text = document.getElementById("yourUTM");
+  Text.select();
+  navigator.clipboard.writeText(Text.value);
 }
